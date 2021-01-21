89085ce699c6111f11afc85fee5b1dfd51d8fc21
GuilhermeWerner <guilhermeqwerner@gmail.com>
2021-01-21 12:35:08 -0300

    Add Sample Functions to Library

--- Source/Library.rs
+++ Source/Library.rs
@@ -1,3 +1,23 @@
-pub fn Hello() {
-    println!("Hello Library");
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+
+#[no_mangle]
+pub extern "C" fn Add(num1: f32, num2: f32) -> f32 {
+    return num1 + num2;
+}
+
+#[no_mangle]
+pub extern "C" fn Subtract(num1: f32, num2: f32) -> f32 {
+    return num1 - num2;
+}
+
+#[no_mangle]
+pub extern "C" fn Multiply(num1: f32, num2: f32) -> f32 {
+    return num1 * num2;
+}
+
+#[no_mangle]
+pub extern "C" fn Divide(num1: f32, num2: f32) -> f32 {
+    return num1 / num2;
 }
