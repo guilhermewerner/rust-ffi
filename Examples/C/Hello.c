89b7844f852f1123a0a1331ab160bb8351c8d5db
GuilhermeWerner <guilhermeqwerner@gmail.com>
2021-01-21 12:47:53 -0300

    Update C and C++ Examples

--- Examples/C/Hello.c
+++ Examples/C/Hello.c
@@ -1,5 +1,3 @@
-// Copyright (c) TribuFu. All Rights Reserved
-
 #include "stdio.h"
 
 #include "Library.h"
@@ -11,24 +9,17 @@ extern float Subtract(float Num1, float Num2);
 
 int main()
 {
-    printf("Rust Calculator\n");
+    printf("Rust Library\n");
 
     int num1 = 1;
     int num2 = 2;
 
-    float added = Add(num1, num2);
-    float subtracted = Subtract(num1, num2);
-    float multiplied = Multiply(num1, num2);
-    float divided = Divide(num1, num2);
+    printf("\nAdded: %f", Add(num1, num2));
+    printf("\nSubtracted: %f", Subtract(num1, num2));
+    printf("\nMultiplied: %f", Multiply(num1, num2));
+    printf("\nDivided: %f", Divide(num1, num2));
 
-    printf("\nAdded: %f", added);
-    printf("\nSubtracted: %f", subtracted);
-    printf("\nMultiplied: %f", multiplied);
-    printf("\nDivided: %f", divided);
-
-    while (1)
-    {
-    }
+    printf("\n\n");
 
     system("PAUSE");
 }
--- Examples/Cpp/Hello.cpp
+++ Examples/Cpp/Hello.cpp
@@ -1,5 +1,3 @@
-// Copyright (c) TribuFu. All Rights Reserved
-
 #include <iostream>
 
 #include "Library.h"
@@ -13,20 +11,17 @@ extern float Subtract(float Num1, float Num2);
 
 int main()
 {
-    cout << "Rust Calculator" << "\n\n";
+    cout << "Rust Library" << "\n\n";
 
     int num1 = 1;
     int num2 = 2;
 
-    float added = Library::Add(num1, num2);
-    float subtracted = Library::Subtract(num1, num2);
-    float multiplied = Library::Multiply(num1, num2);
-    float divided = Library::Divide(num1, num2);
+    cout << "Added: " << Library::Add(num1, num2) << "\n";
+    cout << "Subtracted: " << Library::Subtract(num1, num2) << "\n";
+    cout << "Multiplied: " << Library::Multiply(num1, num2) << "\n";
+    cout << "Divided: " << Library::Divide(num1, num2) << "\n";
 
-    cout << "Added: " << added << "\n";
-    cout << "Subtracted: " << subtracted << "\n";
-    cout << "Multiplied: " << multiplied << "\n";
-    cout << "Divided: " << divided << "\n";
+    cout << "\n";
 
     system("PAUSE");
 }
